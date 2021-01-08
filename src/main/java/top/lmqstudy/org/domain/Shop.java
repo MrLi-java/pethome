00f0302b60fc74b7f5949cd470033967dc3b3560
Mr.Li <320899771@qq.com>
2021-01-08 19:29:09 +0800

    添加门店管理

--- src/main/java/top/lmqstudy/org/domain/Shop.java
+++ src/main/java/top/lmqstudy/org/domain/Shop.java
@@ -18,7 +18,7 @@ public class Shop  extends BaseDomain {
   private String logo;
   private Long admin_id;
 
-  @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+  @JsonFormat(pattern="yyyy-MM-dd",timezone="GMT+8")
   public Date getRegisterTime() {
     return registerTime;
   }
